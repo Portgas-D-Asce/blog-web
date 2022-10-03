57bbaf0d90333b6fa0e1928c7c26eb061139142d
pk <pk@gmail.com>
2022-10-04 02:24:39 +0800

    完成页面footer模块

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,7 +1,22 @@
 <template>
-  <h1>footer</h1>
+  <a class="iconfont icon-github" href="https://github.com/Portgas-D-Asce"></a>
+  <a class="iconfont icon-codeforces" href="https://codeforces.com/profile/Portgas-D-Ace"></a>
+  <div class="copyright">
+     <b>Copyright &copy; {{ date.getFullYear() }} Portgas·D·Asce </b>
+  </div>
 </template>
 <script setup lang="ts">
+import '../assets/fonts/iconfont.css'
+
+const date = new Date();
 </script>
 <style scoped>
+.iconfont {
+  margin-left: 10px;
+  margin-right: 10px;
+}
+.copyright {
+  font-size: 24px;
+  margin-top: 15px;
+}
 </style>
