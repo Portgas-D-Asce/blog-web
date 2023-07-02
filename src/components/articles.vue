1a874a366f59f75d9d09a3fce609b556d8d711c9
Portgasdasce <xxx@163.com>
2023-07-02 18:31:22 +0800

    摘要界面图片显示 bug 修复

--- src/components/articles.vue
+++ src/components/articles.vue
@@ -1,17 +1,20 @@
 <template>
-  <div v-for="article in articles" class="abstract" :style="{background: 'url(https://localhost:8080/blog/api/v1/images?name=' + article.id + '-0.jpg) no-repeat right top', 'background-size': '50%'}">
-    <router-link :to="{ path: '/articles/' + article.id }" class="title"> {{ article.name }} </router-link>
-    <div class="tags">
-      <router-link v-for="tag in article.tags" :to="{ path: '/tag/' + tag.id }" class="tag">
-        <el-tag class="ml-2" :type="get_random_tag_type()"> {{ tag.name }} </el-tag>
-      </router-link>
+    <div v-for="article in articles" class="abstract">
+        <img class="top" :src="`https://localhost:8080/blog/api/v1/images?name=${article.id}-0.jpg`"/>
+        <div class="bottom">
+            <router-link :to="{ path: '/articles/' + article.id }" class="title"> {{ article.name }} </router-link>
+            <div class="tags">
+                <router-link v-for="tag in article.tags" :to="{ path: '/tag/' + tag.id }" class="tag">
+                    <el-tag class="ml-2" :type="get_random_tag_type()"> {{ tag.name }} </el-tag>
+                </router-link>
+            </div>
+            <div class="digest"> {{ article.description ? article.description : "啊，忘了写摘要." }}</div>
+            <div class="statistic">
+                <span class="time">{{ article.date }}</span> 
+                <span style="float: right;">阅读 ({{ article.read }}) | 拉胯 ({{ article.downvoted }}) | 点赞 ({{ article.upvoted }})</span>
+            </div>
+        </div>
     </div>
-    <div class="digest"> {{ article.description ? article.description : "啊，忘了写摘要." }}</div>
-    <div class="statistic">
-      {{ article.date }} 
-      <span style="float:right;">阅读 ({{ article.read }}) | 拉胯 ({{ article.downvoted }}) | 点赞 ({{ article.upvoted }})</span>
-    </div>
-  </div>
 </template>
 
 <script setup lang="ts">
@@ -34,31 +37,36 @@ const get_random_tag_type = () => {
 .abstract {
   background-color: white;
   box-shadow: 1px 1px 10px rgba(0,0,0,.2);
-  padding: 8px 15px;
   border-bottom: 1px solid #eee;
   border-radius: 5px;
-  margin-bottom: 15px;
+  margin-bottom: 30px;
   opacity:0.9;
   background-size: 50%;
-  height: 270px;
+}
+.top {
+  width: 100%;
+}
+.bottom {
+  padding: 8px 15px;
 }
 .title {
   font-size: 3.6vmin;
 }
 .tags {
-  padding-top: 1vmin;
+  padding-top: 20px;
 }
 .tag {
   margin-right: 9px;
 }
 .digest {
   color: #666;
-  font-size: 14px;
-  padding: 10px 0px;
+  font-size: 16px;
+  padding: 20px 0px;
   clear: left;
 }
 .statistic {
   color: #333;
-  font-size: 14px;
+  padding: 20px 0px;
+  font-size: 16px;
 }
 </style>
