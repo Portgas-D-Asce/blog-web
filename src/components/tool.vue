3a4b8c40f2a6a4cb51b17b7cbd5e6c4ada0b4059
Portgas·D·Asce <lupengkunmc@163.com>
2024-01-28 22:01:54 +0800

    test

--- src/components/tool.vue
+++ src/components/tool.vue
@@ -3,7 +3,7 @@
         <div class="tool"><a class="iconfont icon-zhiding" href="#top"></a></div>
         <div class="tool"><router-link to="/categories" class="iconfont icon-sort" href="/category"></router-link></div>
         <div class="tool"><router-link to="/tags" class="iconfont icon-tag" href="/tag"></router-link></div>
-        <div class="tool"><b><a class="iconfont icon-zan"></a></b></div>
+        <!--<div class="tool"><b><a class="iconfont icon-zan"></a></b></div>-->
         <div class="tool"><a class="iconfont icon-home3" href="/blog"></a></div>
     </div>
 </template>
