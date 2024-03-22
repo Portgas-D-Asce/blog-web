a3a5eb1529a2115a8a16580e988511e49cc181d5
Portgas·D·Asce <lupengkunmc@163.com>
2024-03-23 03:04:58 +0800

    test

--- src/components/toc.vue
+++ src/components/toc.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <h2>Toc &#x00A7; &#x00A7;</h2>
+        <h2>Toc &#x00A7;</h2>
         <div id="toc"></div>
     </div>
 </template>
--- src/components/tool.vue
+++ src/components/tool.vue
@@ -3,7 +3,6 @@
         <div class="tool"><a class="iconfont icon-zhiding" href="#top"></a></div>
         <div class="tool"><router-link to="/categories" class="iconfont icon-sort" href="/category"></router-link></div>
         <div class="tool"><router-link to="/tags" class="iconfont icon-tag" href="/tag"></router-link></div>
-        <!--<div class="tool"><b><a class="iconfont icon-zan"></a></b></div>-->
         <div class="tool"><a class="iconfont icon-home3" href="/"></a></div>
     </div>
 </template>
